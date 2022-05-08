4b776c38bc74eff4fe6da0cbb0eb0d7af60de4a3
enrisco <enriquerodrigues790@gmail.com>
2022-05-08 19:29:33 -0300

    0.1.0
    mta coisa de novo fodase

---
+++ projGameJam/Assets/Scripts/CameraController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    Transform target;
+
+    float distance = -10f, height = 0f, damping = 5.0f;
+    public float maxX, maxY, minX, minY;
+
+    public float shakeDuration, shakeMagnetude, shakeMaxX, shakeMinX;
+
+    /*public void ShakeObject()
+    {
+        StartCoroutine(Shake.ShakeObject(shakeDuration, shakeMagnetude, shakeMinX, shakeMaxX, transform));
+    }*/
+
+    void Update()
+    {
+        // get the position of the target (AKA player)
+        Vector3 wantedPosition = target.TransformPoint(0, height, distance);
+
+        // check if it's inside the boundaries on the X position
+        wantedPosition.x = (wantedPosition.x < minX) ? minX : wantedPosition.x;
+        wantedPosition.x = (wantedPosition.x > maxX) ? maxX : wantedPosition.x;
+
+        // check if it's inside the boundaries on the Y position
+        wantedPosition.y = (wantedPosition.y < minY) ? minY : wantedPosition.y;
+        wantedPosition.y = (wantedPosition.y > maxY) ? maxY : wantedPosition.y;
+
+        // set the camera to go to the wanted position in a certain amount of time
+        transform.position = Vector3.MoveTowards(transform.position, wantedPosition, (Time.deltaTime * damping));
+    }
+}
+
--- projGameJam/Assets/Scripts/InteractiveController.cs
+++ projGameJam/Assets/Scripts/InteractiveController.cs
@@ -11,10 +11,12 @@ public class InteractiveController : MonoBehaviour
     [SerializeField] Sprite DesactivedSprite;
 
     SpriteRenderer SpriteRenderer;
+    BoxCollider2D BoxCollider2D;
 
     private void Start()
     {
         SpriteRenderer = GetComponent<SpriteRenderer>();
+        BoxCollider2D = GetComponent<BoxCollider2D>();
     }
 
     public int GetActiveOrDesactiveAndReturnPoints()
@@ -24,4 +26,22 @@ public class InteractiveController : MonoBehaviour
         else SpriteRenderer.sprite = DesactivedSprite;
         return points;
     }
+
+    public void SetActive()
+    {
+        active = !active;
+        if (active) SpriteRenderer.sprite = ActivedSprite;
+        else SpriteRenderer.sprite = DesactivedSprite;
+        BoxCollider2D.enabled = active;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.collider.TryGetComponent<Transform>(out Transform player))
+        {
+            if (player.position.y < transform.position.y) SpriteRenderer.sortingOrder = -1;
+            else SpriteRenderer.sortingOrder = 3;
+        }
+    }
 }
+
