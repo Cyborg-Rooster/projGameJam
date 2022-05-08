e447af77e82f4d8f7d0842a9fb0e02bf1b3f8ab6
enrisco <enriquerodrigues790@gmail.com>
2022-05-08 17:47:52 -0300

    0.0.7
    coisa pra caralho fodase

--- projGameJam/Assets/Scripts/InteractiveController.cs
+++ projGameJam/Assets/Scripts/InteractiveController.cs
@@ -5,4 +5,23 @@ using UnityEngine;
 public class InteractiveController : MonoBehaviour
 {
     public int points;
+    public bool active;
+
+    [SerializeField] Sprite ActivedSprite;
+    [SerializeField] Sprite DesactivedSprite;
+
+    SpriteRenderer SpriteRenderer;
+
+    private void Start()
+    {
+        SpriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    public int GetActiveOrDesactiveAndReturnPoints()
+    {
+        active = !active;
+        if (active) SpriteRenderer.sprite = ActivedSprite;
+        else SpriteRenderer.sprite = DesactivedSprite;
+        return points;
+    }
 }
